7713845fd49f63c710d07d0f55630888cb836735
manu <ivianuu@gmail.com>
2021-08-09 16:43:40 +0200

    Initial commit

---
+++ app/src/main/kotlin/com/ivianuu/mx/GameUi.kt
@@ -0,0 +1,132 @@
+/*
+ * Copyright 2021 Manuel Wrage
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ivianuu.mx
+
+import androidx.compose.animation.*
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.*
+import androidx.compose.ui.graphics.*
+import androidx.compose.ui.res.*
+import androidx.compose.ui.unit.*
+import com.ivianuu.essentials.optics.*
+import com.ivianuu.essentials.store.*
+import com.ivianuu.essentials.ui.animation.*
+import com.ivianuu.essentials.ui.animation.transition.*
+import com.ivianuu.essentials.ui.material.Scaffold
+import com.ivianuu.essentials.ui.material.TopAppBar
+import com.ivianuu.essentials.ui.navigation.*
+import com.ivianuu.injekt.*
+import com.ivianuu.injekt.coroutines.*
+import com.ivianuu.injekt.scope.*
+import kotlinx.coroutines.flow.*
+import kotlin.time.*
+
+@Provide class GameKey : RootKey
+
+@OptIn(ExperimentalAnimationApi::class)
+@Provide val gameUi: ModelKeyUi<GameKey, GameModel> = {
+  Scaffold(
+    topBar = { TopAppBar(title = { Text(stringResource(R.string.app_name)) }) }
+  ) {
+    Column(
+      modifier = Modifier.fillMaxSize(),
+      verticalArrangement = Arrangement.Center,
+      horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+      Row(
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+      ) {
+        Dice(dice = model.dice1, model.dicesVisible)
+
+        Spacer(Modifier.width(8.dp))
+
+        Dice(dice = model.dice2, model.dicesVisible)
+      }
+
+      Spacer(Modifier.height(8.dp))
+
+      Row(
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+      ) {
+        Button(onClick = model.rollDices) {
+          Text(stringResource(R.string.roll_dices))
+        }
+
+        Spacer(Modifier.width(8.dp))
+
+        AnimatedVisibility(model.dice1 != null) {
+          Button(onClick = model.toggleDicesVisibility) {
+            Text(
+              text = stringResource(
+                if (model.dicesVisible) R.string.hide_dices else R.string.show_dices
+              )
+            )
+          }
+        }
+      }
+    }
+  }
+}
+
+@Composable private fun Dice(dice: Int?, visible: Boolean) {
+  AnimatedBox(
+    current = dice to visible,
+    transition = CrossFadeStackTransition(defaultAnimationSpec(120.milliseconds))
+  ) { (currentDice, currentVisible) ->
+    Box(
+      modifier = Modifier
+        .size(128.dp)
+        .border(1.dp, Color.Black),
+      contentAlignment = Alignment.Center
+    ) {
+      Text(if (currentVisible) currentDice?.toString() ?: "?" else "?")
+    }
+  }
+}
+
+@Optics data class GameModel(
+  val dice1: Int? = null,
+  val dice2: Int? = null,
+  val dicesVisible: Boolean = false,
+  val rollDices: () -> Unit = {},
+  val toggleDicesVisibility: () -> Unit = {}
+)
+
+@Provide fun gameModel(
+  scope: InjektCoroutineScope<KeyUiScope>
+): @Scoped<KeyUiScope> StateFlow<GameModel> = scope.state(GameModel()) {
+  action(GameModel.rollDices()) {
+    val diceResult1 = (Math.random() * 6 + 1).toInt()
+    val diceResult2 = (Math.random() * 6 + 1).toInt()
+
+    val dice1 = if (diceResult2 > diceResult1) diceResult2 else diceResult1
+    val dice2 = if (diceResult2 > diceResult1) diceResult1 else diceResult2
+
+    update {
+      copy(dice1 = dice1, dice2 = dice2, dicesVisible = true)
+    }
+  }
+
+  action(GameModel.toggleDicesVisibility()) {
+    update { copy(dicesVisible = !dicesVisible) }
+  }
+}
---
+++ app/src/main/kotlin/com/ivianuu/mx/MxTheme.kt
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2021 Manuel Wrage
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ivianuu.mx
+
+import androidx.compose.runtime.*
+import androidx.compose.ui.graphics.*
+import com.ivianuu.essentials.*
+import com.ivianuu.essentials.twilight.domain.*
+import com.ivianuu.essentials.twilight.ui.*
+import com.ivianuu.essentials.ui.*
+import com.ivianuu.essentials.ui.animation.transition.*
+import com.ivianuu.essentials.ui.material.colors
+import com.ivianuu.injekt.*
+import kotlinx.coroutines.flow.*
+
+object MxTheme {
+  val Primary = Color(0xFFEF5350)
+  val Secondary = Color(0xFFF49C17)
+}
+
+@Provide fun mxTheme(twilightState: StateFlow<TwilightState>): AppTheme = { content ->
+  TwilightTheme(
+    lightColors = colors(
+      isLight = true,
+      primary = MxTheme.Primary,
+      primaryVariant = MxTheme.Primary,
+      secondary = MxTheme.Secondary,
+      secondaryVariant = MxTheme.Secondary
+    ),
+    darkColors = colors(
+      isLight = false,
+      primary = MxTheme.Primary,
+      primaryVariant = MxTheme.Primary,
+      secondary = MxTheme.Secondary,
+      secondaryVariant = MxTheme.Secondary
+    ),
+    blackColors = colors(
+      isLight = false,
+      primary = MxTheme.Primary,
+      primaryVariant = MxTheme.Primary,
+      secondary = MxTheme.Secondary,
+      secondaryVariant = MxTheme.Secondary,
+      background = Color.Black,
+      surface = Color.Black
+    ),
+    twilightState = twilightState.collectAsState().value
+  ) {
+    CompositionLocalProvider(
+      LocalStackTransition provides ScaledSharedAxisStackTransition(),
+      content = content
+    )
+  }
+}
