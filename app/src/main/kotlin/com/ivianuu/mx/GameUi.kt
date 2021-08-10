5a911eeea07f1853befbb9e741a77c83817c3e2f
manu <ivianuu@gmail.com>
2021-08-10 18:52:28 +0200

    Better dice rendering

--- app/src/main/kotlin/com/ivianuu/mx/GameUi.kt
+++ app/src/main/kotlin/com/ivianuu/mx/GameUi.kt
@@ -24,6 +24,7 @@ import androidx.compose.foundation.shape.*
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.*
+import androidx.compose.ui.geometry.*
 import androidx.compose.ui.graphics.*
 import androidx.compose.ui.res.*
 import androidx.compose.ui.unit.*
@@ -115,7 +116,199 @@ import kotlin.time.*
         .clickable {},
       contentAlignment = Alignment.Center
     ) {
-      Text(if (currentVisible) currentDice?.toString() ?: "?" else "?")
+      if (currentDice == null || !currentVisible) {
+        Text(
+          text = "?",
+          fontSize = 36.sp
+        )
+      } else {
+        DiceContent(currentDice)
+      }
+    }
+  }
+}
+
+@Composable private fun DiceContent(dice: Int) {
+  val dotColor = LocalContentColor.current
+  Canvas(Modifier.fillMaxSize()) {
+    val dotRadius = 8.dp.toPx()
+
+    when (dice) {
+      1 -> {
+        drawCircle(color = dotColor, radius = dotRadius)
+      }
+      2 -> {
+        drawCircle(
+          color = dotColor,
+          radius = dotRadius,
+          center = Offset(
+            x = size.width * 0.25f,
+            y = size.height * 0.75f
+          )
+        )
+
+        drawCircle(
+          color = dotColor,
+          radius = dotRadius,
+          center = Offset(
+            x = size.width * 0.75f,
+            y = size.height * 0.25f
+          )
+        )
+      }
+      3 -> {
+        drawCircle(
+          color = dotColor,
+          radius = dotRadius,
+          center = Offset(
+            x = size.width * 0.25f,
+            y = size.height * 0.75f
+          )
+        )
+
+        drawCircle(color = dotColor, radius = dotRadius)
+
+        drawCircle(
+          color = dotColor,
+          radius = dotRadius,
+          center = Offset(
+            x = size.width * 0.75f,
+            y = size.height * 0.25f
+          )
+        )
+      }
+      4 -> {
+        drawCircle(
+          color = dotColor,
+          radius = dotRadius,
+          center = Offset(
+            x = size.width * 0.25f,
+            y = size.height * 0.25f
+          )
+        )
+
+        drawCircle(
+          color = dotColor,
+          radius = dotRadius,
+          center = Offset(
+            x = size.width * 0.75f,
+            y = size.height * 0.25f
+          )
+        )
+
+        drawCircle(
+          color = dotColor,
+          radius = dotRadius,
+          center = Offset(
+            x = size.width * 0.25f,
+            y = size.height * 0.75f
+          )
+        )
+
+        drawCircle(
+          color = dotColor,
+          radius = dotRadius,
+          center = Offset(
+            x = size.width * 0.75f,
+            y = size.height * 0.75f
+          )
+        )
+      }
+      5 -> {
+        drawCircle(
+          color = dotColor,
+          radius = dotRadius,
+          center = Offset(
+            x = size.width * 0.25f,
+            y = size.height * 0.25f
+          )
+        )
+
+        drawCircle(
+          color = dotColor,
+          radius = dotRadius,
+          center = Offset(
+            x = size.width * 0.75f,
+            y = size.height * 0.25f
+          )
+        )
+
+        drawCircle(
+          color = dotColor,
+          radius = dotRadius,
+          center = Offset(
+            x = size.width * 0.25f,
+            y = size.height * 0.75f
+          )
+        )
+
+        drawCircle(
+          color = dotColor,
+          radius = dotRadius,
+          center = Offset(
+            x = size.width * 0.75f,
+            y = size.height * 0.75f
+          )
+        )
+
+        drawCircle(color = dotColor, radius = dotRadius)
+      }
+      6 -> {
+        drawCircle(
+          color = dotColor,
+          radius = dotRadius,
+          center = Offset(
+            x = size.width * 0.25f,
+            y = size.height * 0.2f
+          )
+        )
+
+        drawCircle(
+          color = dotColor,
+          radius = dotRadius,
+          center = Offset(
+            x = size.width * 0.75f,
+            y = size.height * 0.2f
+          )
+        )
+
+        drawCircle(
+          color = dotColor,
+          radius = dotRadius,
+          center = Offset(
+            x = size.width * 0.25f,
+            y = size.height * 0.5f
+          )
+        )
+
+        drawCircle(
+          color = dotColor,
+          radius = dotRadius,
+          center = Offset(
+            x = size.width * 0.75f,
+            y = size.height * 0.5f
+          )
+        )
+
+        drawCircle(
+          color = dotColor,
+          radius = dotRadius,
+          center = Offset(
+            x = size.width * 0.25f,
+            y = size.height * 0.8f
+          )
+        )
+
+        drawCircle(
+          color = dotColor,
+          radius = dotRadius,
+          center = Offset(
+            x = size.width * 0.75f,
+            y = size.height * 0.8f
+          )
+        )
+      }
+      else -> throw AssertionError("Unexpected dice $dice")
     }
   }
 }
